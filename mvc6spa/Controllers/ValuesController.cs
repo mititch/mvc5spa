1925bb0502fbc8599c2ea28460770de169b75753
Saha Mititch <mititch@live.com>
2015-05-12 10:48:20 +0300

    start

---
+++ mvc6spa/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿namespace mvc6spa.Controllers
+{
+    using System.Web.Http;
+
+    public class ValuesController : ApiController
+    {
+        public IHttpActionResult GetValues()
+        {
+            return Ok(new[] { "a", "b", "c" });
+        }
+    }
+}
